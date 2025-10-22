c7e016f7d1989bad3d7e8a2bcfa44d6abcc202ca
Martina Flores <145231552+Martanga02@users.noreply.github.com>
2025-10-21 23:12:24 -0300

    Delete tp_parte2 directory

--- tp_parte2/app/views/auth.view.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class AuthView {
-
-    function showFormLogin($error = null) {
-        // la variable $error ahora se pasa directamente al archivo .phtml
-        require 'templates/formLogin.phtml';
-    }
-}
